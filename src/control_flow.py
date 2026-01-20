d680bba7e2c0e085e0bba73a164ffdb48ad2c7c4
MJ-DE17 <manasadevi.j@diggibyte.com>
2026-01-20 19:15:58 +0530

    Added Three python files

---
+++ src/control_flow.py
@@ -0,0 +1,14 @@
+
+# if-else example
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("Positive number")
+elif number == 0:
+    print("Zero")
+else:
+    print("Negative number")
+
+# loop example
+for i in range(1, 4):
+    print("Count:", i)
